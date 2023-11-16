6df3134e5cc1da5c58ffac4e9e59f0489eb45715
MinJi Kim (Mindal) <108729047+mjkim0819@users.noreply.github.com>
2023-11-16 23:33:14 +0900

    Update target_number.py

---
+++ DFS&BFS/target_number.py
@@ -0,0 +1,18 @@
+def solution(numbers, target):
+    answer = 0
+    queue = []
+    
+    length = len(numbers)
+    queue.append([-numbers[0], 0])
+    queue.append([+numbers[0], 0])
+    
+    while queue :
+        num, i = queue.pop()
+        if i+1 == length :
+            if num == target :
+                answer += 1
+        else :
+            queue.append([num - numbers[i + 1], i + 1])
+            queue.append([num + numbers[i + 1], i + 1])
+    
+    return answer
